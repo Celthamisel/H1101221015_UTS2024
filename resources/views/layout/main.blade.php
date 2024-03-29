669afa0ad9280bf36549887931b33f3033828e31
Celthamisel <celthamissella13@gmail.com>
2024-03-29 19:56:37 +0700

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="./assets/img/favicon.png">
   <title>
-    Argon Dashboard 2 by Creative Tim
+    Sistem Informasi Kepegawaian Universitas Tanjungpura
   </title>
   @include('layout.partial.link')
 </head>
