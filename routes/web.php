b0a3952a435a8876fbcd8e64a7626b4145656f57
Celthamisel <celthamissella13@gmail.com>
2024-03-29 13:23:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
 });
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
